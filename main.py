17ed38ea695431f98c30bf0819f9765e299a4577
Ramiro Uffelmann <ramirouf@gmail.com>
2023-07-07 23:26:40 -0300

    Finished 3rd module (validations with pydantic)

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
-from fastapi import FastAPI, Body
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Body, Path, Query, status, Response
+from fastapi.responses import HTMLResponse, JSONResponse
+from pydantic import BaseModel, Field
+from typing import Optional, List
+import datetime
 
 app = FastAPI()
 app.title = "My app with FastAPI"
 app.version = "0.0.1"
+
+
+class Movie(BaseModel):
+    id: Optional[int] = None
+    title: str = Field(min_length=5, max_length=20)
+    year: int = Field(le=datetime.date.today().year)
+    category: str = Field(min_length=5, max_length=20)
+
+    # The following replaces the use of "default" as a parameter in each Field()
+    class Config:
+        schema_extra = {
+            "example": {
+                "id": 0,
+                "title": "Unknown title",
+                "year": 2023,
+                "category": "Unknown category",
+            }
+        }
+
+
 movies = [
     {"id": 1, "title": "Avatar", "year": 2009, "category": "Action"},
     {"id": 2, "title": "Interstellar", "year": 2014, "category": "Sci-fi"},
@@ -16,54 +39,71 @@ def message():
     return HTMLResponse("<h1>Hello world!</h1>")
 
 
-@app.get("/movies", tags=["movies"])
-def get_movies():
+# response_model is used to define and document the schema (structure) of the response
+@app.get(
+    "/movies",
+    tags=["movies"],
+    response_model=List[Movie],
+    status_code=status.HTTP_200_OK,
+)
+def get_movies() -> List[Movie]:
     return movies
+    # Both do the same, because FastAPI by default sends an JSONResponse with the content of the return
+    # The following is a redundant way
+    # return JSONResponse(content=movies)
 
 
-@app.get("/movies/{id}", tags=["movies"])
-def get_movie(id: int):
+@app.get(
+    "/movies/{id}",
+    tags=["movies"],
+    response_model=Movie,
+    status_code=status.HTTP_200_OK,
+)
+# Path is used to validate the path parameter
+def get_movie(response: Response, id: int = Path(ge=1, le=2000)) -> Movie:
     for item in movies:
         if item["id"] == id:
             return item
-    return []
+    return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content=[])
+    # response.status_code = status.HTTP_404_NOT_FOUND
+    # return []
 
 
 # Query params have key value
-@app.get("/movies/", tags=["movies"])
+@app.get("/movies/", tags=["movies"], response_model=List[Movie])
 # Last slash used to declare that a query param will be received
 # Filter movies by category
-def get_movies_by_category(category: str, year: int):
-    # FastAPI detects that category and year are query params
-    for item in movies:
-        if item["category"] == category and item["year"] == year:
-            return item
-    return []
+# ↓↓↓ Query is used because a query param is received !!! ↓↓↓
+def get_movies_by_category(
+    category: str = Query(min_length=5, max_length=15)
+) -> List[Movie]:
+    # ↑↑↑ FastAPI detects that category and year are query params ↑↑↑
+    return [item for item in movies if item["category"] == category]
+    # for item in movies:
+    #     if item["category"] == category and item["year"] == year:
+    #         return item
+    # return []
 
 
-@app.post("/movies", tags=["movies"])
-def create_movie(
-    id: int = Body(), title: str = Body(), year: int = Body(), category: str = Body()
-):
-    movies.append({"id": id, "title": title, "year": year, "category": category})
-    return movies
+@app.post("/movies", tags=["movies"], response_model=dict, status_code=201)
+def create_movie(movie: Movie) -> dict:
+    movies.append(movie)
+    return {"message": "The movie was registered"}
 
 
-@app.put("/movies/{id}", tags=["movies"])
-def update_movie(
-    id: int, title: str = Body(), year: int = Body(), category: str = Body()
-):
+@app.put("/movies/{id}", tags=["movies"], response_model=dict)
+def update_movie(id: int, movie: Movie) -> dict:
     for item in movies:
         if item["id"] == id:
-            item["title"] = title
-            item["year"] = year
-            item["category"] = category
-            return movies
+            item["title"] = movie.title
+            item["year"] = movie.year
+            item["category"] = movie.category
+            return {"message": "The movie was updated"}
 
 
-@app.delete("/movies/{id}", tags=["movies"])
-def delete_movie(id: int):
+@app.delete("/movies/{id}", tags=["movies"], response_model=dict)
+def delete_movie(id: int) -> dict:
     for item in movies:
         if item["id"] == id:
             movies.remove(item)
-            return movies
+            return {"message": "The movie was deleted"}
